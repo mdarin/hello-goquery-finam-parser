9d3f8173255187497f8343ed80bbc0fd841647a8
darin-m <darin.m@tvzavr.ru>
2019-04-16 16:47:40 +0300

    разбор продвигается далее

--- src/main.go
+++ src/main.go
@@ -55,6 +55,9 @@ func keepLines(s string, n int) string {
 }
 
 func GetAssetsList() {
+	var CSSPath string
+
+/*
   // Request the HTML page.
   res, err := http.Get("https://www.finam.ru/quotes/stocks/russia/")
   if err != nil {
@@ -71,64 +74,129 @@ func GetAssetsList() {
     log.Fatal(err)
   }
 
-	// Grabbed document
-	//TODO:read doc: https://godoc.org/github.com/PuerkitoBio/goquery 
-	// isn't work
-	//fmt.Println("document:",doc)
 
-	var CSSPath string
 
-	//CSSPath = "html"
+	 Find the table items
+  doc.Find(CSSPath).Each(func(i int, s *goquery.Selection) {
+    // For each item found, get the band and title
+		f := s.Find("td").Text()
+		fmt.Println("f:", f)
+  })
+
+*/
 
 	CSSPath = "html body.i-user_client_no.i-user_client_no div table tbody"
 
+	stop := false
+	for i := 1; i < 100 && !stop; i++ {
+		fmt.Println()
+		fmt.Println("  POST  page ",i)
+		fmt.Println()
 
-	// Find the table items
-  doc.Find(CSSPath).Each(func(i int, s *goquery.Selection) {
-    // For each item found, get the band and title
-		f := s.Find("td").Text()
-		fmt.Println("f:", f)
-  })
+		// convert to string
+		page := fmt.Sprintf("%d", i)
 
+		//We can use POST form to get result, too.
+		res, err := http.PostForm("https://www.finam.ru/quotes/stocks/russia/",
+			url.Values{"pageNumber": {page}})
+		if err != nil {
+			//panic(err)
+			log.Fatal(err)
+		}
+		defer res.Body.Close()
+		if res.StatusCode != 200 {
+			//log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+			stop = true
+		} else {
+			// Load the HTML document
+			doc, err := goquery.NewDocumentFromReader(res.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-	fmt.Println()
-	fmt.Println("  POST  page 1")
-	fmt.Println()
+			// Find the table items
+			doc.Find(CSSPath).Each(func (i int, s *goquery.Selection) {
+				// For each item found, get the band and title
+				s.Find("tr").Each(func (i int, s1 *goquery.Selection) {
+					href,_ := s1.Find("a").Attr("href")
+					title,_ := s1.Find("a").Attr("title")
+					fmt.Println("href:", href)
+					fmt.Println("title:", title)
 
-	//We can use POST form to get result, too.
-	res, err = http.PostForm("https://www.finam.ru/quotes/stocks/russia/",
-		url.Values{"pageNumber": {"1"}})
-	if err != nil {
-		//panic(err)
-    log.Fatal(err)
+					s1.Find("td").Each( func (i int, s2 *goquery.Selection) {
+						f := s2.Find("span").Text()
+						fmt.Println("f:", f)
+					})
+				})
+			})
+		}
 	}
-	defer res.Body.Close()
-  if res.StatusCode != 200 {
-    log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-  }
-
-  // Load the HTML document
-  doc, err = goquery.NewDocumentFromReader(res.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-	// Find the table items
-  doc.Find(CSSPath).Each(func(i int, s *goquery.Selection) {
-    // For each item found, get the band and title
-		f := s.Find("td").Text()
-		fmt.Println("f:", f)
-  })
 
 }
+
+
+func GetAssetHistory() {
+//	CSSPath := "html body.i-user_logged_no.i-user_client_no div.finam-wrap div.finam-global-container div.content div.layout table.main tbody tr td#content-block.inside-container.content div#issuer-profile div#issuer-profile-container div#issuer-profile-outer div#issuer-profile-inner div#issuer-profile-content div#issuer-profile-export div#issuer-profile-export-form form#chartform.i-form-state"
+
+
+	CSSPath := "html body.i-user_logged_no.i-user_client_no div.finam-wrap div.finam-global-container div.content div.layout table.main tbody tr td#content-block.inside-container.content div#issuer-profile div#issuer-profile-container div#issuer-profile-outer div#issuer-profile-inner div#issuer-profile-content div#issuer-profile-export div#issuer-profile-export-form"
+
+
+	//We can use POST form to get result, too.
+	res, err := http.PostForm("https://www.finam.ru/profile/moex-akcii/sberbank/export",
+		nil)//url.Values{"pageNumber": {page}})
+	if err != nil {
+		//panic(err)
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	} else {
+		// Load the HTML document
+		doc, err := goquery.NewDocumentFromReader(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Find the table items
+		doc.Find(CSSPath).Each(func (i int, s *goquery.Selection) {
+			// For each item found
+			id,_ := s.Find(CSSPath + " form").Attr("id")
+			class,_ := s.Find(CSSPath + " form").Attr("class")
+			name,_ := s.Find(CSSPath + " form").Attr("name")
+			action,_ := s.Find(CSSPath + " form").Attr("action")
+			method,_ := s.Find(CSSPath + " form").Attr("method")
+			fmt.Println("id:",id)
+			fmt.Println("class:",class)
+			fmt.Println("name:",name)
+			fmt.Println("action:",action)
+			fmt.Println("method:",method)
+			s.Find(CSSPath + " form input").Each(func (i int, s1 *goquery.Selection) {
+				id,_ := s1.Attr("id")
+				//typ,_ := s1.Attr("type")
+				name,_ := s1.Attr("name")
+				value,_ := s1.Attr("value")
+				fmt.Println(" *****")
+				fmt.Println(" id:",id)
+				//fmt.Println(" type:",typ)
+				fmt.Println(" name:",name)
+				fmt.Println(" value:",value)
+			})
+		})
+	}
+}
+
 
 //
 // main driver
 //
 func main() {
-	ExampleScrape()
+//	ExampleScrape()
 
-	GetAssetsList()
+	//GetAssetsList()
+
+	GetAssetHistory()
 /*
 	// Go contains rich function for grab web contents. net/http is the major library
 	// https://dlintw.github.io/gobyexample/public/http-client.html
