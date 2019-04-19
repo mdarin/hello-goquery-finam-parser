d778ef9df7b83e97d220d386ae61abd85de20899
darin-m <darin.m@tvzavr.ru>
2019-04-19 18:14:53 +0300

    реализован обход всех инструментов и подготовлена загрузка истории

--- src/main.go
+++ src/main.go
@@ -5,23 +5,42 @@
 package main
 
 import (
-  "fmt"
-  "log"
+	// common purpose
+	"fmt"
+	"log"
 
-  "net/http"
+	// statndard
+	"net/http"
 	"net/url"
 	"io/ioutil"
 	"strings"
 	"regexp"
+	"os" // for operations with dirs
+	//NOTE: The path package should only be used for paths separated by forward slashes, 
+	//      such as the paths in URLs. This package does not deal with Windows paths with 
+	//      drive letters or backslashes; to manipulate operating system paths, use the path/filepath package.
+	"path"
+
+	// Package filepath implements utility routines for manipulating filename paths in a way
+	// compatible with the target operating system-defined file paths.
+	"path/filepath"
 
 	// page parser
-  "github.com/PuerkitoBio/goquery"
-
+	"github.com/PuerkitoBio/goquery"
 	// toUtf8 conversin
 	// https://stackoverflow.com/questions/6927611/go-language-how-to-convert-ansi-text-to-utf8
 	"golang.org/x/text/encoding/charmap"
 )
 
+//TODO: обязательно добавить таймауты на заросы!!
+
+const(
+
+)
+
+var(
+	counter = 0
+)
 
 // Win1251toUtf8 and Win1251fromUtf8 conversin
 // https://stackoverflow.com/questions/6927611/go-language-how-to-convert-ansi-text-to-utf8
@@ -31,7 +50,7 @@ func DecodeWindows1251(enc []byte) string {
 	return string(out)
 }
 
-func EncodeWindows1250(inp string) []byte {
+func EncodeWindows1251(inp string) []byte {
 	enc := charmap.Windows1251.NewEncoder()
 	out, _ := enc.String(inp)
 	// For converting from a string to a byte slice, string -> []byte:
@@ -40,34 +59,6 @@ func EncodeWindows1250(inp string) []byte {
 	return []byte(out)
 }
 
-//
-// пример использования библиотеки goquery
-//
-func ExampleScrape() {
-  // Request the HTML page.
-  res, err := http.Get("http://metalsucks.net")
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer res.Body.Close()
-  if res.StatusCode != 200 {
-    log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-  }
-
-  // Load the HTML document
-  doc, err := goquery.NewDocumentFromReader(res.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  // Find the review items
-  doc.Find(".sidebar-reviews article .content-block").Each(func(i int, s *goquery.Selection) {
-    // For each item found, get the band and title
-    band := s.Find("a").Text()
-    title := s.Find("i").Text()
-    fmt.Printf("Review %d: %s - %s\n", i, band, title)
-  })
-}
 
 
 func keepLines(s string, n int) string {
@@ -78,10 +69,6 @@ func keepLines(s string, n int) string {
 
 //TODO: inprove
 func getAssetsList() {
-	var CSSPath string
-
-	CSSPath = "html body.i-user_client_no.i-user_client_no div table tbody"
-
 	stop := false
 	for i := 1; i < 100 && !stop; i++ {
 
@@ -116,33 +103,37 @@ func getAssetsList() {
 				log.Fatal(err)
 			}
 
+			CSSPath := "html body.i-user_client_no.i-user_client_no div table tbody"
+
 			// Find the table items
-			doc.Find(CSSPath).Each(func (i int, s *goquery.Selection) {
+			doc.Find(CSSPath).Each(func (i int, tbody *goquery.Selection) {
 				// For each item found, get the band and title
-				s.Find("tr").Each(func (i int, s1 *goquery.Selection) {
-					href,_ := s1.Find("a").Attr("href")
-					title,_ := s1.Find("a").Attr("title")
+				tbody.Find("tr").Each(func (i int, tr *goquery.Selection) {
+					// получить ссылку и наименование актива
+					href,_ := tr.Find("a").Attr("href")
+					title,_ := tr.Find("a").Attr("title")
+					//convert to utf-8
+					title = DecodeWindows1251([]byte(title))
 					fmt.Println()
-					fmt.Println("href:", href)
-					fmt.Println("title:", DecodeWindows1251([]byte(title)))
+					fmt.Println("tr href:", href)
+					fmt.Println("tr title:", title)
 					// цифры
-					//s1.Find("td").Each( func (i int, s2 *goquery.Selection) {
-					//	f := s2.Find("span").Text()
-					//	fmt.Println("f:", f)
+					//tr.Find("td").Each( func (i int, td *goquery.Selection) {
+					//	spanValue := td.Find("span").Text()
+					//	fmt.Println("span value:", spanValue)
 					//})
+					//получить параметры актива перейдя по ссылке и выбрав требуемые значения полей
+					getAssetParams(href,title)
 				})
 			})
 		}
 	}
 }
 
-func readTable() {
-
-}
-
 /*
 Как получить элемент с помощью jQuery?
-Для того чтобы понимать как работает селектор Вам все-же необходимы базовые знания CSS, т.к. именно от принципов CSS отталкивается селектор jQuery:
+Для того чтобы понимать как работает селектор Вам все-же необходимы базовые знания CSS, т.к. 
+именно от принципов CSS отталкивается селектор jQuery:
 
 $(“#header”) – получение элемента с id=”header”
 $(“h3”) – получить все <h3> элементы
@@ -150,20 +141,33 @@ $(“div#content .photo”) – получить все элементы с кл
 $(“ul li”) – получить все <li> элементы из списка <ul>
 $(“ul li:first”) – получить только первый элемент <li> из списка <ul>
 */
+//func accessElem() {
+//}
 
 // получить параметры по каждому интсрументу
-//func accessElem() {
-func getAssetParams() {
+func getAssetParams(href,title string) {
+
+	fmt.Println(" ::href:",href)
+	fmt.Println(" ::title:",title)
+
+	counter++
+	fmt.Println(" ::count:",counter)
+
+	// сформировать запросо
+	req := path.Join("www.finam.ru",href,"export")
+	req = "https://" + req
+	fmt.Println(" ::req:",req)
+
 	//We can use POST form to get result, too.
-	res, err := http.PostForm("https://www.finam.ru/profile/moex-akcii/lukoil/export",
-		nil)//url.Values{"pageNumber": {page}})
+	//res, err := http.PostForm("https://www.finam.ru/profile/moex-akcii/lukoil/export",nil)
+	res, err := http.PostForm(req,nil)
 	if err != nil {
 		//panic(err)
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		//log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 	} else {
 		// Load the HTML document
 		doc, err := goquery.NewDocumentFromReader(res.Body)
@@ -171,38 +175,82 @@ func getAssetParams() {
 			log.Fatal(err)
 		}
 
+		// параметры для формирования запроса
+		params := make(map[string]string)
 		// Find the table items
-		doc.Find("div#issuer-profile-export-form").Each(func (i int, s *goquery.Selection) {
+		doc.Find("div#issuer-profile-export-form").Each(func (i int, form *goquery.Selection) {
 			// For each item found
-			id,_ := s.Find("form").Attr("id")
-			class,_ := s.Find("form").Attr("class")
-			fmt.Println("id:",id)
-			fmt.Println("class:",class)
-			s.Find("form#"+id+"."+class+" input").Each(func (i int, s1 *goquery.Selection) {
-				id1,_ := s1.Attr("id")
-				fmt.Println(" id1:",id1)
-				name,_ := s.Find("#"+id1).Attr("name")
-				fmt.Println("  name:", name)
-				value,_ := s.Find("#"+id1).Attr("value")
-				fmt.Println("  value:", value)
+			formId,_ := form.Find("form").Attr("id")
+			formClass,_ := form.Find("form").Attr("class")
+			fmt.Println("form id:",formId)
+			fmt.Println("form class:",formClass)
+			form.Find("form#"+formId+"."+formClass+" input").Each(func (i int, input *goquery.Selection) {
+				inputId,_ := input.Attr("id")
+				fmt.Println(" input id:",inputId)
+				inputName,_ := form.Find("#"+inputId).Attr("name")
+				fmt.Println(" input name:", inputName)
+				inputValue,_ := form.Find("#"+inputId).Attr("value")
+				fmt.Println(" input value:", inputValue)
+				// записать параметр
+				params[inputName] = inputValue
 			})
-			s.Find("form#"+id+"."+class+" select").Each(func (i int, s1 *goquery.Selection) {
-				id2,_ := s1.Attr("id")
-				fmt.Println(" id2:",id2)
-				s.Find("#"+id2+" option").Each(func (i int, s2 *goquery.Selection) {
-					value,_ := s2.Attr("value")
-					fmt.Println("  value:", value)
+			form.Find("form#"+formId+"."+formClass+" select").Each(func (i int, sel *goquery.Selection) {
+				selectId,_ := sel.Attr("id")
+				fmt.Println(" select:",selectId)
+				form.Find("#"+selectId+" option").Each(func (i int, option *goquery.Selection) {
+					value,_ := option.Attr("value")
+					fmt.Println(" select value:", value)
 				})
 			})
-			s.Find("form#"+id+"."+class+" tbody tr").Each(func (i int, s1 *goquery.Selection) {
-				id3,_ := s1.Attr("id")
-				if id3 != "" {
-					fmt.Println(" id3:",id3)
-					text := s.Find("#"+id3+" td").First().Text()
-					fmt.Println("text:",DecodeWindows1251([]byte(text)))
+			form.Find("form#"+formId+"."+formClass+" tbody tr").Each(func (i int, tr *goquery.Selection) {
+				trId,_ := tr.Attr("id")
+				// костыль, в выводе есть пустой тэг tr, это условие его фильтрует
+				if trId != "" {
+					fmt.Println(" tr id:",trId)
+					text := form.Find("#"+trId+" td").First().Text()
+					fmt.Println(" tr text:",DecodeWindows1251([]byte(text)))
 				}
 			})
 		})
+
+		// задать натройки по умолчанию либо жёстко заданные
+		//params["market"] = "1"
+		//params["em"] = "8"
+		//params["code"] = "LKOH"
+		// неизветный
+		params["apply"] = "0"
+		//параметры времени.
+		//params["df"] = "1"
+		//params["mf"] = "1"
+		//params["yf"] = "2018"
+		//params["from"] = "01.01.2018"
+		//params["dt"] = "17"
+		//params["mt"] = "4"
+		//params["yt"] = "2019"
+		//params["to"] = "17.04.2019"
+		//период котировок
+		//params["p"] = "8" // дни
+		//расширение получаемого файла
+		params["e"] = ".csv"
+		//формат даты
+		params["dtf"] = "1"
+		//формат времени
+		params["tmf"] ="1"
+		//выдавать время
+		params["MSOR"] = "0" // MSOR0 ??
+		params["mstimever"] = "1" // MSOR1 ??
+		params["mstime"] = "on" // пустое значение приходит
+		//параметр разделитель полей
+		params["sep"] = "3"
+		//параметр разделитель разрядов
+		params["sep2"] = "2"
+		//Перечень получаемых данных
+		params["datf"] = "1"
+		//добавлять заголовок в файл
+		params["at"] = "1"
+
+		// загрузить истоирю инструмента с указанными параметрами	
+		downloadAssetHistory(params)
 	}
 }
 
@@ -307,7 +355,97 @@ at — добавлять заголовок в файл (
 		0 — нет, 
 		1 — да)
 */
-func downloadAssetHistory() {
+func downloadAssetHistory(params map[string]string) {
+
+	// map traversal
+	for k,v := range params {
+		fmt.Println("parameter ",k,":",v)
+	}
+
+
+	// общие параметры
+	market := params["market"]//"1"
+	em := params["em"]//"8"
+	code := params["code"]//"LKOH"
+	// неизветный
+	apply := params["apply"]//"0"
+	//параметры времени.
+	df := params["df"]//"1"
+	mf := params["mf"]//"1"
+	yf := params["yf"]//"2018"
+	from := params["from"]//"01.01.2018"
+	dt := params["dt"]//"17"
+	mt := params["mt"]//"4"
+	yt := params["yt"]//"2019"
+	to := params["to"]//"17.04.2019"
+	//период котировок
+	p := params["p"]//"8" // дни
+	//расширение получаемого файла
+	e := params["e"]//".csv"
+	//формат даты
+	dtf := params["dtf"]//"1"
+	//формат времени
+	tmf :=params["tmf"]//"1"
+	//выдавать время
+	MSOR := params["MSOR"]//"0"
+	mstimever := params["mstimever"]//"1"
+	mstime := params["mstime"]//"on"
+	//параметр разделитель полей
+	sep := params["sep"]//"3"
+	//параметр разделитель разрядов
+	sep2 := params["sep2"]//"2"
+	//Перечень получаемых данных
+	datf := params["datf"]//"1"
+	//добавлять заголовок в файл
+	at := params["at"]//"1"
+	// наименование выходного файла
+	// https://golang.org/pkg/regexp/#pkg-examples
+	re := regexp.MustCompile(`(?P<day>[0-9]+)[.](?P<month>[0-9]+)[.][0-9]{2,2}(?P<year>[0-9]{2,2})`)
+	fromPart := fmt.Sprintf("${%s}${%s}${%s}", re.SubexpNames()[3], re.SubexpNames()[2], re.SubexpNames()[1])
+	toPart := fmt.Sprintf("${%s}${%s}${%s}", re.SubexpNames()[3], re.SubexpNames()[2], re.SubexpNames()[1])
+	fromName := re.ReplaceAllString(from, fromPart)
+	toName := re.ReplaceAllString(to, toPart)
+	f := code + "_" + fromName + "_" + toName
+
+	//TODO: можно собрать и из хэша, но надо аккуратно см.пробник mapEx()
+	// запрос истории иснтрумента с указанными параметрами
+	req := "http://export.finam.ru/"+f+e+"?market="+market+"&em="+em+"&code="+code+"&apply="+apply+"&df="+df+"&mf="+mf+"&yf="+yf+"&from="+from+"&dt="+dt+"&mt="+mt+"&yt="+yt+"&to="+to+"&p="+p+"&f="+f+"&e="+e+"&cn="+code+"&dtf="+dtf+"&tmf="+tmf+"&SOR="+MSOR+"&mstime="+mstime+"&mstimever="+mstimever+"&sep="+sep+"&sep2="+sep2+"&datf="+datf+"&at="+at
+	fmt.Println("request:",req)
+
+/*
+	// Request the HTML page.
+	res, err := http.Get(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	} else {
+		body,_ := ioutil.ReadAll(res.Body)
+		fmt.Println("GET:")
+		//fmt.Println(keepLines(string(body)))
+		fmt.Println(string(body))
+	}
+*/
+}
+
+
+/*
+Алгоритм
+
+надо будет разобраться со структурой чтобы по каталогам это всё аккуратно заложить
+
+
+TODO: для каждого рынка[Акции,Облигации?] 
+	для каждого иструмента 
+		-получить страницу для загрузки данных истории
+		-на странице загрузки итории получить параметры требуемы для загрузки
+		-сформировать запрос для загрузки данных и загрузить данные истории
+*/
+
+func downloadEx() {
+
 	// общие параметры
 	market := "1"
 	em := "8"
@@ -365,15 +503,15 @@ func downloadAssetHistory() {
 	fmt.Println("request:",req)
 
 
-  // Request the HTML page.
-  res, err := http.Get(req)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer res.Body.Close()
-  if res.StatusCode != 200 {
-    log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-  } else {
+	// Request the HTML page.
+	res, err := http.Get(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	} else {
 		body,_ := ioutil.ReadAll(res.Body)
 		fmt.Println("GET:")
 		//fmt.Println(keepLines(string(body)))
@@ -382,17 +520,95 @@ func downloadAssetHistory() {
 
 }
 
+func dirEx() {
+	// fetching pwd
+	workdir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("working dir is:",workdir)
+	// existance and filepath joining
+	filename := filepath.Join(workdir,"a-nonexistent-file.txt")
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		fmt.Println("file \""+filename+"\" does not exist")
+	}
+}
 
-/*
-	Алгоритм
-	TODO: для каждого рынка[Акции,Облигации?] 
-		для каждого иструмента 
-			-получить страницу для загрузки данных истории
-			-на странице загрузки итории получить параметры требуемы для загрузки
-			-сформировать запрос для загрузки данных и загрузить данные истории
-*/
+func mapEx() {
+	params := make(map[string]string)
+	params["market"] = "1"
+  params["em"] = "8"
+  params["code"] = "LKOH"
+  // неизветный
+  params["apply"] = "0"
+  //параметры времени.
+  params["df"] = "1"
+  params["mf"] = "1"
+  params["yf"] = "2018"
+  params["from"] = "01.01.2018"
+  params["dt"] = "17"
+  params["mt"] = "4"
+  params["yt"] = "2019"
+  params["to"] = "17.04.2019"
+  //период котировок
+  params["p"] = "8" // дни
+  //расширение получаемого файла
+  params["e"] = ".csv"
+  //формат даты
+  params["dtf"] = "1"
+  //формат времени
+  params["tmf"] ="1"
+  //выдавать время
+  params["MSOR"] = "0"
+  params["mstimever"] = "1"
+  params["mstime"] = "on"
+  //параметр разделитель полей
+  params["sep"] = "3"
+  //параметр разделитель разрядов
+  params["sep2"] = "2"
+  //Перечень получаемых данных
+  params["datf"] = "1"
+  //добавлять заголовок в файл
+  params["at"] = "1"
+	re := regexp.MustCompile(`(?P<day>[0-9]+)[.](?P<month>[0-9]+)[.][0-9]{2,2}(?P<year>[0-9]{2,2})`)
+	fromPart := fmt.Sprintf("${%s}${%s}${%s}", re.SubexpNames()[3], re.SubexpNames()[2], re.SubexpNames()[1])
+	toPart := fmt.Sprintf("${%s}${%s}${%s}", re.SubexpNames()[3], re.SubexpNames()[2], re.SubexpNames()[1])
+	fromName := re.ReplaceAllString(params["from"], fromPart)
+	toName := re.ReplaceAllString(params["to"], toPart)
+	t := params["code"] + "_" + fromName + "_" + toName
+	params["f"] = t + params["e"]
 
+	// сформировать запрос истории иснтрумента с указанными параметрами
+	req := "http://export.finam.ru/" + t
+	sep := "?"
+	// Map traversal
+	// The for...range loop statement can be used to walk the content of a map value
+	for k,v := range params {
+		// технически можно, но надо внимательно следить, не все пары совпадают
+		// например, &SOR=0 в настоящем запроса
+		// а у тут &MSOR=0
+		// в целом выгрузка происходит, 
+		// но это опасненько, надо отлаживать
+		req += sep + k + "=" + v
+		sep = "&"
+	}
+	fmt.Println("request:",req)
 
+	// Request the HTML page.
+	res, err := http.Get(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	} else {
+		body,_ := ioutil.ReadAll(res.Body)
+		fmt.Println("GET:")
+		//fmt.Println(keepLines(string(body)))
+		fmt.Println(string(body))
+	}
+}
 
 //
 // main driver
@@ -400,8 +616,12 @@ func downloadAssetHistory() {
 func main() {
 
 	getAssetsList()
-	getAssetParams()
-	downloadAssetHistory()
+	//getAssetParams()
+	//downloadAssetHistory()
+
+	//downloadEx()
+	dirEx()
+	//mapEx()
 
 }
 
@@ -490,3 +710,31 @@ func GetAssetHistory() {
 	}
 }
 */
+//
+// пример использования библиотеки goquery
+//
+func ExampleScrape() {
+  // Request the HTML page.
+  res, err := http.Get("http://metalsucks.net")
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer res.Body.Close()
+  if res.StatusCode != 200 {
+    log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+  }
+
+  // Load the HTML document
+  doc, err := goquery.NewDocumentFromReader(res.Body)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // Find the review items
+  doc.Find(".sidebar-reviews article .content-block").Each(func(i int, s *goquery.Selection) {
+    // For each item found, get the band and title
+    band := s.Find("a").Text()
+    title := s.Find("i").Text()
+    fmt.Printf("Review %d: %s - %s\n", i, band, title)
+  })
+}
